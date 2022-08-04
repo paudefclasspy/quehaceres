8e6962f9a655888c6f6ca986c797a1fa31f32be9
paudefclasspy <pauaranega20@gmail.com>
2022-08-04 16:25:59 +0200

    added models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tarea(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, blank=True)
+    title = models.CharField(max_length=200,blank=True)
+    description = models.TextField(max_length=400,blank=True)
+    complete = models.BooleanField(default=False)
